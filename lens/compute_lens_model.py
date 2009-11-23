93931f8c0392e4d15fc4e2794d3c57e3095c4e2d
Pedro C. Ferreira <pedro@if.ufrj.br>
2009-11-23 18:34:39 -0200

    Deletd blank lines to allow doxygen correct documentation

--- lens/compute_lens_model.py
+++ lens/compute_lens_model.py
@@ -1,26 +1,18 @@
 
 
 ##@package compute_lens_model [formerly kappalens]
-
 # converts halo model to lens model. if halo is a surface density map, the lens model will be a kappa map. 
 #
-
 # here, the lens model is the list of projected NFW parameters kappas and xsL 
 #@param zl 
 #@param zs
-
 #@param halo_model (m200, c200,el,thetal) 
 #@param cosmological_background_parameters (Omega_m, Omega_l)
-
 #@return Return a dictionary with the lens model parameters
-
 def compute_lens_model (zl, zs, halo_model, cosmological_background_parameters):
-
     import ksmod as KSmod # import KSmod (Gabriel's modules); 
-
     m200=halo_model['m200'] 
     c200=halo_model['c200']
-
     el=halo_model['el'] 
     thetal=halo_model['thetal'] 
     Omega_m = cosmological_background_parameters['Omega_m'] 
@@ -31,7 +23,6 @@ def compute_lens_model (zl, zs, halo_model, cosmological_background_parameters):
     xsL = 60* KSmod.x_s(zl,m200,c200,Omega_m,Omega_l) # NFW xsL calculated by Gabriel's module. Converted from arcmin to arcsec
     #------------------------------------------------------------------------------
     log('Obtained kappas = %f and xsL = %f from Gabriel\'s module\n' % (kappas, xsL) )
-
     # creates a dictionary 
     return({'kappas':kappas, 'xsL':xsL,'el':el,'thetal':thetal})   
 
