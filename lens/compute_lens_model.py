cdc661ca5ffa67e80fb4577906a94ac58b74d8b1
Pedro C. Ferreira <pedro@if.ufrj.br>
2009-12-10 18:37:58 -0200

    updated ksmod name on compute_lens_model

--- lens/compute_lens_model.py
+++ lens/compute_lens_model.py
@@ -1,5 +1,4 @@
 
-
 ##@package compute_lens_model [formerly kappalens]
 # converts halo model to lens model. if halo is a surface density map, the lens model will be a kappa map. 
 #
@@ -11,7 +10,7 @@
 #@return Return a dictionary with the lens model parameters
 def compute_lens_model (zl, zs, halo_model, cosmological_background_params):
 	from arcgeneratormodules import log
-        import ksmod as KSmod # import KSmod (Gabriel's modules); 
+        import compute_NFW_lens_parameters as KSmod # import KSmod (Gabriel's modules); 
         m200=halo_model['m200'] 
         c200=halo_model['c200']
         el=halo_model['el'] 
@@ -25,6 +24,6 @@ def compute_lens_model (zl, zs, halo_model, cosmological_background_params):
         #------------------------------------------------------------------------------
         log('Obtained kappas = %f and xsl = %f from Gabriel\'s module\n' % (kappas, xsl) )
         # creates a dictionary 
-        return({'kappas':kappas, 'xsl':xsl,'el':el,'thetal':thetal})   
+        return {'kappas':kappas, 'xsl':xsl,'el':el,'thetal':thetal} 
 
 
