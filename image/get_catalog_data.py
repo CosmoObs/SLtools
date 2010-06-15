b486ed4a726dbffd1c8c7e5cd554ff7172707a0c
Angelo Fausti Neto <fausti@fausti-laptop.(none)>
2010-06-15 14:34:27 -0300

    Adding get_catalog_data

---
+++ image/get_catalog_data.py
@@ -0,0 +1,37 @@
+'''@file 
+get_catalog_data module
+'''
+
+
+'''@package get_catalog_data 
+Get a list of variables from a FITS catalog 
+'''
+import pyfits 
+def get_catalog_data( file_name, *args ): 
+
+	''' Get a list of variables from a FITS catalog
+
+	@param file_name: name of the FITS catalog 
+	@param *args is a comma separated list of variables to be read 
+
+	@return a dictionary with the variable names and values
+	'''
+	
+	if ( len(args) == 0 ): 
+		return (None); 
+
+	hdulist = pyfits.open(file_name, ignore_missing_end = True)
+	tbdata = hdulist[1].data; 
+
+	dic = {}; 
+	for arg in args: 
+		try:
+			dic[arg] = tbdata.field(arg); 
+		except (KeyError):
+			print "Variable %s does not exist in this catalog." % arg
+			
+	return (dic); 
+
+
+	
+
