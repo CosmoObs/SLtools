86aa403dd8922a28d65c8d97b9a0a4aa10ff034c
gbcaminha <gbcaminha@gmail.com>
2010-10-06 14:21:43 -0300

    .

--- PerturbativeMethod/nfw_circular_model.h
+++ PerturbativeMethod/nfw_circular_model.h
@@ -155,7 +155,7 @@ double re_find_func_nfw(double r, void *p){
 
 //pot_params[0] = rs
 //pot_params[1] = kappas
-double r_e_nfw_find(double pot_params[], double *est_err_out=NULL, double x_lo = 1E-4, double x_hi = 10.0, int max_iter = 100, double relative_error = 1E-4, int v=0){
+double r_e_nfw_find(double pot_params[], double *est_err_out=NULL, double x_lo = 1E-4, double x_hi = 10.0, int max_iter = 100, double relative_error = 1E-4, int v=1){
   double *params = (double*) malloc(2.0*sizeof(double));
   params[0] = pot_params[0];
   params[1] = pot_params[1];
