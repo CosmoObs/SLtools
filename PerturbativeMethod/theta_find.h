19831a002facca3a16817203db25b22b47ae6b83
Gabriel Caminha <gbcaminha@wintermute.(none)>
2010-10-13 20:49:18 -0300

    some modifications in theta_find.h

--- PerturbativeMethod/theta_find.h
+++ PerturbativeMethod/theta_find.h
@@ -63,5 +63,65 @@ void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_p
     }
   }
 
+  fclose(pFile);
+
+}
+
+void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_params[], double out[2][10], int N=1000){
+
+  double theta[N];
+  double arg[N];
+  int N_pair=0;
+
+  for(int i=0;i<N;i++) theta[i] = double(i)*2.0*3.141592654/double(N);
+  for(int i=0;i<N;i++) arg[i] = arg_sqrt(Df0Dtheta_in, source_in, theta[i], pert_params);
+
+
+  for(int i=0;i<N;i++){
+    if(arg[i]*arg[i+1]  < 0){
+      //printf("%f  %f  %f\n",theta[i], arg[i],arg[i+1]);
+      N_pair++;
+    }
+  }
+  double theta_N[N_pair+1];
+  int count = 0;
+  for(int i=0;i<N;i++){
+    if(arg[i]*arg[i+1]  < 0){
+      theta_N[count] = theta[i];
+      count++;
+    }
+  }
+
+  theta_N[N_pair] =  theta_N[0];
+  
+  double test = 0;
+  test=arg_sqrt(Df0Dtheta_in, source_in, (theta_N[0]+theta_N[1])/2.0, pert_params);
+
+
+  
+//  FILE * pFile = fopen ("theta_out.txt","w");
+//  fprintf(pFile,"%i\n",N_pair/2);
+ out[0][0] = int(N_pair/2);
+ out[0][1] = 2.0*3.141592654/double(N);
+
+  int n_pos = 1;
+  if(test>0){
+    for(int i=0;i<N_pair;i+=2){
+      //fprintf(pFile,"%f %f\n",theta_N[i],theta_N[i+1]);
+      out[n_pos][0] = theta_N[i];
+      out[n_pos][1] = theta_N[i+1];
+      n_pos++;
+    }
+  } else {
+    for(int i=0;i<N_pair;i+=2){
+      //fprintf(pFile,"%f %f\n",theta_N[i+1],theta_N[i+2]);
+      out[n_pos][0] = theta_N[i+1];
+      out[n_pos][1] = theta_N[i+2];
+      n_pos++;
+    }
+  }
+
+//  fclose(pFile);
+
 }
 #endif
