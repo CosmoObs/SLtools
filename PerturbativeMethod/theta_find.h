20c6677d7ec875b8579f80fe22ddd2073107ef01
HabibDumet <habib@habib-desktop.(none)>
2010-10-15 19:31:26 -0300

    Verified and corrected version of theta_find.h

--- PerturbativeMethod/theta_find.h
+++ PerturbativeMethod/theta_find.h
@@ -18,14 +18,14 @@
 
 #include "perturbative_method.h"
 
-void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_params[], int N=1000){
+void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_params[], double _r_e, int N=1000){
 
   double theta[N];
   double arg[N];
   int N_pair=0;
 
   for(int i=0;i<N;i++) theta[i] = double(i)*2.0*3.141592654/double(N);
-  for(int i=0;i<N;i++) arg[i] = arg_sqrt(Df0Dtheta_in, source_in, theta[i], pert_params);
+  for(int i=0;i<N;i++) arg[i] = arg_sqrt(Df0Dtheta_in, source_in, theta[i], pert_params,_r_e);
 
 
   for(int i=0;i<N;i++){
@@ -46,7 +46,7 @@ void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_p
   theta_N[N_pair] =  theta_N[0];
   
   double test = 0;
-  test=arg_sqrt(Df0Dtheta_in, source_in, (theta_N[0]+theta_N[1])/2.0, pert_params);
+  test=arg_sqrt(Df0Dtheta_in, source_in, (theta_N[0]+theta_N[1])/2.0, pert_params,_r_e);
 
 
   
@@ -67,14 +67,14 @@ void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_p
 
 }
 
-void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_params[], double out[10][2], int N=1000){
+void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_params[], double _r_e, double out[10][2], int N=1000){
 
   double theta[N];
   double arg[N];
   int N_pair=0;
 
   for(int i=0;i<N;i++) theta[i] = double(i)*2.0*3.141592654/double(N);
-  for(int i=0;i<N;i++) arg[i] = arg_sqrt(Df0Dtheta_in, source_in, theta[i], pert_params);
+  for(int i=0;i<N;i++) arg[i] = arg_sqrt(Df0Dtheta_in, source_in, theta[i], pert_params,_r_e);
 
 
   for(int i=0;i<N;i++){
@@ -95,33 +95,54 @@ void theta_find(f_type Df0Dtheta_in, elliptical_source source_in,  double pert_p
   theta_N[N_pair] =  theta_N[0];
   
   double test = 0;
-  test=arg_sqrt(Df0Dtheta_in, source_in, (theta_N[0]+theta_N[1])/2.0, pert_params);
+  test=arg_sqrt(Df0Dtheta_in, source_in, (theta_N[0]+theta_N[1])/2.0, pert_params,_r_e);
 
 
   
 //  FILE * pFile = fopen ("theta_out.txt","w");
 //  fprintf(pFile,"%i\n",N_pair/2);
+ double d_theta = 2.0*3.141592654/double(N);
  out[0][0] = int(N_pair/2);
- out[0][1] = 2.0*3.141592654/double(N);
+ out[0][1] = d_theta;
 
   int n_pos = 1;
   if(test>0){
     for(int i=0;i<N_pair;i+=2){
       //fprintf(pFile,"%f %f\n",theta_N[i],theta_N[i+1]);
+      double test_inf = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i], pert_params,_r_e);
+      while(test_inf<0){
+	theta_N[i]+=d_theta;
+	test_inf = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i], pert_params,_r_e);
+      }
       out[n_pos][0] = theta_N[i];
+      double test_sup = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+1], pert_params,_r_e);
+      while(test_sup<0){
+	theta_N[i+1]-=d_theta;
+	test_sup = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+1], pert_params,_r_e);
+      }      
       out[n_pos][1] = theta_N[i+1];
       n_pos++;
     }
   } else {
     for(int i=0;i<N_pair;i+=2){
       //fprintf(pFile,"%f %f\n",theta_N[i+1],theta_N[i+2]);
+      double test_inf = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+1], pert_params,_r_e);
+      while(test_inf<0){
+	theta_N[i+1]+=d_theta;
+	test_inf = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+1], pert_params,_r_e);
+      }
       out[n_pos][0] = theta_N[i+1];
+      double test_sup = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+2], pert_params,_r_e);
+      while(test_sup<0){
+	theta_N[i+2]-=d_theta;
+	test_sup = arg_sqrt(Df0Dtheta_in, source_in, theta_N[i+2], pert_params,_r_e);
+      }
       out[n_pos][1] = theta_N[i+2];
       n_pos++;
     }
   }
 
-//  fclose(pFile);
+//  Corrected values
 
 }
 #endif
