85afce41c6d158e24141c52bf5a59782603009e5
HabibDumet <habib@habib-desktop.(none)>
2010-10-28 14:52:51 -0200

    Latest version for some functions and examples

---
+++ PerturbativeMethod/examples/external_shear_teste2.cpp
@@ -0,0 +1,105 @@
+#include <cstdlib>
+#include <cstdio>
+
+#include "../perturbative_method.h"
+#include "../pnfw_model.h"
+#include "../external_shear.h"
+#include "../theta_find.h"
+
+void print_arcs_exsh(elliptical_source source_in, double pot_params[], double pet_params[], int npts=1000){
+
+  double theta = 0.0;
+  double r_p = 0.0;
+  double r_m = 0.0;
+// Added by Habib Dumet-Montoya
+  double r_tcrit=0.0;
+  double y1_caust=0.0;
+  double y2_caust=0.0;
+  double src_y1=0.0;
+  double src_y2=0.0;
+
+//pot_paramas[0]=r_s, pot_params[1]=ks,pot_params[2]=elipticity,
+  //double pot_params[] = {0.9,1.0,0.2};
+// Added by Habib Dumet-Montoya
+FILE *outarc = fopen ("arcs_nfw_exsh.dat" , "w");
+FILE *oute = fopen ("einstein_circle.dat" , "w");
+FILE *outtc = fopen ("tang_crit_exsh.dat" , "w");
+FILE *outcau = fopen ("tang_caust_exsh.dat" , "w");
+FILE *outsrc = fopen ("src_plot_exsh.dat" , "w");
+
+    for(int i=0;i<=npts;i++){
+
+      if(arg_sqrt(df0dtheta_exsh, source_in, theta, pot_params)>0.0){
+      r_p = _r_e+dr_plus(f1_exsh,df0dtheta_exsh,kappa2_nfw(pot_params), source_in, theta, pot_params);
+      r_m = _r_e+dr_minus(f1_exsh,df0dtheta_exsh,kappa2_nfw(pot_params), source_in, theta, pot_params);
+//       printf("%E %E\n",r_p*cos(theta),r_p*sin(theta));
+//       printf("%E %E\n",r_m*cos(theta),r_m*sin(theta));
+      fprintf(outarc,"%E %E\n",r_p*cos(theta),r_p*sin(theta)); // printing to a file
+      fprintf(outarc,"%E %E\n",r_m*cos(theta),r_m*sin(theta)); // printing to a file
+    }
+// Added by Habib: The tangential Critical Curve.
+// Printing the Einstein Ring to a File
+     fprintf(oute,"%E %E\n",_r_e*cos(theta),_r_e*sin(theta)); 
+//  Defining the tangential critical radius
+     r_tcrit= r_crit(f1_exsh,ddf0dtheta_exsh,kappa2_nfw(pot_params),theta,pot_params); 
+// Writing in a file the tangential critical curve
+     fprintf(outtc,"%E %E\n",r_tcrit*cos(theta),r_tcrit*sin(theta));   
+//  Defining the polar equation of the tangential caustic
+     y1_caust=caustic_y1(df0dtheta_exsh,ddf0dtheta_exsh,theta,pot_params);
+     y2_caust=caustic_y2(df0dtheta_exsh,ddf0dtheta_exsh,theta,pot_params);
+//   Writing in a file the tangential caustic line
+     fprintf(outcau,"%E %E\n",y1_caust,y2_caust); 
+//   Writing the source in a file 
+     fprintf(outsrc,"%E %E\n",y1_src(source_in, theta),y2_src(source_in, theta));  
+//
+    theta+= 6.283185308/npts;
+  }
+}
+
+int main(){
+
+// Added by Habib Dumet-Montoya
+  FILE *outls = fopen ("lensing_data.dat" , "w");  
+
+  int npts = 1000;
+
+  double twpi= 6.283185308;  
+  double pot_params[] = {1.5,0.4,0.0};
+  double s_ext_params[]= {0.1,0.02,0.0}; // parametros de perturbação externa com shear
+
+  elliptical_source source;
+
+  source.x0 = (r_e_nfw(pot_params)/10);
+  source.y0 = 0.0;
+  source.R0 = (sqrt(2.0)/50.0)*r_e_nfw(pot_params);
+  source.eta0 = 0.0;
+  source.theta0 = 0.0;
+
+  //_r_e = 1.1172980994467465;
+
+  
+// Added by Habib Dumet-Montoya
+  fprintf(outls," NFW + External Shear Model Parameters: \n");  
+  fprintf(outls," Scale Radius = %f\n",pot_params[0]);
+  fprintf(outls," Characteristic Convergence = %f\n",pot_params[1]);
+  fprintf(outls," Ellipticity of the Lensing Potential =  %f\n\n",pot_params[2]);
+  fprintf(outls," Source Data: \n");  
+  fprintf(outls," Source Position %f %f\n",source.x0,source.y0);
+  fprintf(outls," Source Radius %f\n",source.R0);
+  fprintf(outls," Source Ellipticity and Inclination %f %f\n\n",source.eta0,source.theta0);
+
+  //print_arcs_nfw(source,npts);
+
+  //for(int i=0;i<=1000;i++) printf("%E %E\n",0.9+0.5*double(i)/1000.0, lambda_t(0.9+0.5*double(i)/1000.0,pot_params));
+
+  double *out = (double*)malloc(2.0*sizeof(double));
+  bracketing_lambda_t( lambda_t_nfw, pot_params, out);
+  printf("# Range of the root = %E  %E\n",out[0], out[1] );
+  double z_out=  root_find(alpha_nfw, shear_nfw, pot_params,out[0] ,out[1]);
+  fprintf(outls," Einstein Radius = %E\n",z_out);
+  _r_e = z_out;
+//   _r_e=r_e_nfw(pot_params);
+  //   fprintf(outls, "Einstein Radius= %f\n",r_e_nfw(pot_params));
+  theta_find(df0dtheta_exsh, source,pot_params );
+  print_arcs_exsh(source,pot_params,s_ext_params,npts);
+}
---
+++ PerturbativeMethod/examples/pe_sis_test.cpp
@@ -0,0 +1,61 @@
+#include <cstdlib>
+#include <cstdio>
+#include <fstream>        //funcoes de entrada e saida para arquivos
+//#include <cstdio>
+#include <iostream>
+
+#include "../perturbative_method.h"
+#include "../psis_model.h"
+#include "../sis_model.h"
+#include "../theta_find.h"
+#include "../generate_arcs.h"
+#include "../generate_curves.h"
+#include "../arc_properties.h" 
+#define TAM_MAX 20
+
+
+// To compile: g++ -o exec_name pe_sis_test.cpp `pkg-config gsl --cflags --libs`
+// To running: exec_name
+
+
+int main(){
+  
+  FILE *outarc = fopen ("arcs_pesis.dat" , "w");
+  FILE *outls = fopen ("lensing_data_pesis.txt" , "w");  
+  FILE *outtc = fopen ("tang_crit_pesis.dat" , "w");
+  FILE *outcau = fopen ("tang_caust_pesis.dat" , "w");
+  FILE *outsrc = fopen ("src_plot_pesis.dat" , "w");
+
+   int npts = 4000;
+
+  double twpi= 6.283185308;  
+//   double pert_params[]={0.64,1.5, 0.1};
+//   double pot_params[]={pert_params[0],pert_params[1],pert_params[2]};
+  double pert_params[]={0.1,3,0.64};
+  double pot_params[] = {pert_params[2]};
+  
+  double _r_e_sis= pot_params[0];
+  printf("Einstein Radius = %f\n",_r_e_sis);
+  double kappa_2 = kappa2_sis(pot_params,_r_e_sis);
+
+  elliptical_source source;
+
+  source.x0 = (_r_e_sis/10.);
+  source.y0 = source.x0;
+  printf(" Source Position %f %f\n",source.x0, source.y0);
+  source.R0 = (sqrt(2.0)/25.0)*_r_e_sis;
+  source.eta0 = 0.0;
+  source.theta0 = twpi/8.0;
+
+  fprintf(outls," PE_SIS Model Parameters: \n");  
+  fprintf(outls," Mass = %f\n",pot_params[0]);
+  fprintf(outls," Ellipticity of the Lensing Potential =  %f\n\n",pert_params[0]);
+  fprintf(outls," Source Data: \n");  
+  fprintf(outls," Source Position %f %f\n",source.x0,source.y0);
+  fprintf(outls," Source Radius %f\n",source.R0);
+  fprintf(outls," Source Ellipticity and Inclination %f %f\n\n",source.eta0,source.theta0);
+
+  plot_arcs(source, f1_psis, Df0Dtheta_psis, pert_params, kappa_2,_r_e_sis,npts, outarc);
+  plot_curves(source, f1_psis, Df0Dtheta_psis, D2f0Dtheta2_psis, pert_params, kappa_2,_r_e_sis, npts, outtc, outcau, outsrc);
+  arc_measures(f1_psis, Df0Dtheta_psis, kappa_2, source, pert_params, _r_e_sis, npts,outls);   
+}
