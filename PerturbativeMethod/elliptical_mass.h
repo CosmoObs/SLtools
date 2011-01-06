7627216d688e3cc7dcdb510460223bf335bc18c8
Gabriel Caminha <gbcaminha@wintermute.(none)>
2011-01-06 13:31:55 -0200

    .

--- PerturbativeMethod/elliptical_mass.h
+++ PerturbativeMethod/elliptical_mass.h
@@ -7,9 +7,9 @@
 
 // g++ -Wall ellip_mass.cpp `pkg-config gsl --cflags --libs`
 
-double f_1_ellip_mass(conv_type conv_in,double X1, double X2, double conv_params[], double r_e, double a, double b){
-  double theta = atan(X2/X1);
-  //double r = sqrt(X1*X1 + X2*X2);
+double f_1_ellip_mass(conv_type conv_in,double theta, double conv_params[], double r_e, double a, double b){
+  double X1 = r_e*cos(theta);
+  double X2 = r_e*sin(theta);
 
   double J0 = J(0, conv_in, X1, X2, conv_params, a, b);
   double J1 = J(1, conv_in, X1, X2, conv_params, a, b);
