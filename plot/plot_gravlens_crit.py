0602f778c49db815058d5330e4f6f677243ed74c
Angelo Fausti Neto <fausti@fausti-laptop.(none)>
2011-02-01 03:02:36 -0200

    Updated plot_gravlens_crit documentation, make it an executable module and added testing with an example

--- plot/plot_gravlens_crit.py
+++ plot/plot_gravlens_crit.py
@@ -1,55 +1,102 @@
+#!/usr/bin/env python
+
+"""Module to plot caustics and critical curves from gravlens"""
+
 ##@package plot_gravlens_crit
 #
-#@param file_name
-#@return -
+# Executable package: YES
+#
+# This module uses sltools.geometry.separate_curves to separate radial and tangential curves
+#
+# To see the package help message, type:
+#
+# > python plot_gravlens_crit --help
+#
+# The input of 'plot_gravlens_crit'  is a gravlens file created by the 'plotcrit' command and plotmode 1
+#
+# The result is a png file with the caustics and critical curves.
+#
+# Example:
+# > python plot_gravlens_crit.py -i testing/curves.txt
 
-# ---
-import matplotlib as mplib;
-from matplotlib.pyplot import plot, show
-from numpy import loadtxt, where, unique1d, array
-# ---
-def plot_gravlens_crit( file_name=None ):
 
-    if file_name is None:
-        file_name='cc.dat'
-	
-    x, y, u, v = loadtxt(file_name, unpack=True)
-    delta_curve = 1 # maximum distance between consecutive points in gravlens output (~ gridhi1/ngrid1) definining a curve
+from matplotlib.pyplot import *
+from numpy import * 
+from random import *
+import sys
 
-    start_point=list()
-    end_point=list()
+from sltools.geometry import separate_curves
 
-    i=0 # points
-    while i < (len(x)- 1):
-	
-        start_point.append(i)
 
-        while (i < (len(x) - 1)) and ((pow(x[i+1]-x[i],2)+pow(y[i+1]-y[i],2)) < delta_curve): 
-            i=i+1
+def plot_gravlens_crit(file):
+    '''Function to plot gravlens caustics and critical curves
+    @param input_file: gravlens gravlens file created by the 'plotcrit' command and plotmode 1
+    '''
 
+    # The gravlens plotmode 1 output are consecutive line segments defining a set of 2D curves 
+
+    x1, y1, u1, v1, x2, y2, u2, v2 = loadtxt(file, unpack=True)
+
+    colors='rygbrygbrygb'
+
+    clf() # Clear previous plot
+    axes().set_aspect('equal')
+
+    # Plot critical curves
+
+    subplot(121)
+    title("Critical curves")
+
+    curves = separate_curves(x1, y1, x2, y2)
+    i=0
+
+    for curve in curves:
+        x1, y1, x2, y2 = curve
+        plot([x1, x2], [y1, y2], color = colors[i])  # plot line segments from (x1,y1) to (x2,y2)
         i=i+1
-        end_point.append(i)
 
-    print start_point
-    print end_point
+    # Plot caustics
 
-    print "# curves ", len(start_point)
-    # Image Plane
+    subplot(122)
+    title("Caustics")
 
-    for j in range(len(start_point)):
-        plot(x[start_point[j]:end_point[j]],y[start_point[j]:end_point[j]])
+    curves = separate_curves(u1, v1, u2, v2)
+    i=0
 
-    show()
-    # Source Plane 
+    for curve in curves:
+        u1, v1, u2, v2 = curve
+        plot([u1, u2], [v1, v2], color = colors[i])
+        i=i+1
 
-    for j in range(len(start_point)):
-        plot(u[start_point[j]:end_point[j]],v[start_point[j]:end_point[j]])
+    savefig(file + '.png')            
 
-    show()
+    return 0
 
-# -
-
+# \cond
 if __name__ == "__main__":
-    import sys
-    plot_gravlens_crit(sys.argv[1])
 
+    from optparse import OptionParser;
+    parser = OptionParser();
+
+    parser.add_option('-i','--input_file',
+            dest='file', default=None,
+            help='Gravlens plotcrit output with plotmode 1');
+
+    (opts,args) = parser.parse_args();
+    file = opts.file;
+
+    if ( opts=={} ):
+      print"";
+      parser.print_help();
+      print ""; 
+      sys.exit(1);
+    if ( file==None ):
+      print ""; 
+      parser.print_help();
+      print ""; 
+      sys.exit(1);
+
+    plot_gravlens_crit(file)
+
+    print "Created %s file." % (file + '.png')
+# \endcond
