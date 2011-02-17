0de5355377f74580c34d97d9ee849160a9d62f54
HabibDumet <habib@habib-desktop.(none)>
2011-02-17 17:19:03 -0200

    falling the module

--- gravlens/find_CC_new.py
+++ gravlens/find_CC_new.py
@@ -296,7 +296,7 @@ def run_find_CC(lens_model, mass_scale, model_param_8, model_param_9, model_para
 		np.savetxt(rad_curves_file,(rad_CC_x,rad_CC_y,rad_caustic_x, rad_caustic_y))
 #	"imprimir os arquivos rad_cc_x, rad_cc_y, rad_caustic_x, rad_caustic_y in to a file rad_curves.txt"
 
-#	return rad_CC_x, rad_CC_y, tan_CC_x, tan_CC_y, rad_caustic_x, rad_caustic_y, tan_caustic_x, tan_caustic_y
+	return rad_CC_x, rad_CC_y, tan_CC_x, tan_CC_y, rad_caustic_x, rad_caustic_y, tan_caustic_x, tan_caustic_y
 
 
 
