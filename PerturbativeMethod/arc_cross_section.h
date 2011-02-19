0c635bcc1dd3f40ba074e757b348bdafb138ee7f
HabibDumet <habib@habib-desktop.(none)>
2011-02-19 17:53:46 -0200

    last changes

--- PerturbativeMethod/arc_cross_section.h
+++ PerturbativeMethod/arc_cross_section.h
@@ -27,4 +27,9 @@ double sigma_defarcs(f_type f1_in, f_type D2f0Dtheta2_in, double pert_params[], 
   return kappa2*kappa2*rth_fact*out;
 }
 
+double sigma_defarcs_sis(double _r_e, double r_th){
+double Pi = 3.1415926535897932384626433832795;
+double factor=(fabs(r_th*r_th)+1)/pow(fabs(r_th*r_th)-1 ,2);
+return 2.*Pi*_r_e*_r_e*factor;
+}
 #endif 
