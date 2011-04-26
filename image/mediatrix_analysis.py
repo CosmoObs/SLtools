062e62c6b9a92ae80d60615aef6e655a5eed5b7b
ClecioBom <clecio@debom.com.br>
2011-04-25 22:53:23 -0300

    Correct import on mediatrix analisys code

--- image/mediatrix_analysis.py
+++ image/mediatrix_analysis.py
@@ -6,7 +6,7 @@ from math import sin, cos ,sqrt, fabs, atan
 from pylab import *
 import pyfits
 from scipy.optimize import fmin
-from mediatrix_decomposition import lenght
+from mediatrix_decomposition import get_length, print_mediatrix_Object_graph
 
 def Sum_inner_product(mediatrix_data):
 
@@ -60,36 +60,36 @@ def Evaluate_S_Statistic(mediatrix_data, sigma_out=True,sigma=1,sigma_pre=0.5,Ar
     Function to calculate the S estatistic measurements.
     
     Input:
-     - mediatrix_data <list> : a list of dictionary structure. Each list item is a dictionary with information of corresponding to a mediatrix vector. The keys are 'theta' for the angle with x axis, 'linear_coefficient' for the linear coefficient from the line in the vector direction, 'origin' the point (x,y) of the vector origin, 'end' the point (x,y) of the vector, 'modulus' for vector modulus. The first item from the list has an extra key 'id' wich contains the image file name. It is the output from Mediatrix_Decomposition.
+     - mediatrix_data <dic> : 
 
      
     Output:
      - <dic> : dictionary with the S statistic measurements. The keys are  
      
     """
-    guess=mediatrix_data[0]['center']
+    guess=mediatrix_data['center']
     L=0
     theta=[]
     linear=[]
     for i in range(0,len(mediatrix_data)):
-        theta.append(mediatrix_data[i]['theta'])
-        linear.append(mediatrix_data[i]['linear_coefficient'])
-        L=L+mediatrix_data[i]['modulus']
+        theta.append(mediatrix_data['theta'][i])
+        linear.append(mediatrix_data['linear_coefficient'][i])
+        L=L+mediatrix_data['modulus'][i]
         
     MinM=fmin(M_function,guess,args=(theta,linear),maxiter=1000, disp=0)
     MinM_val=M_function(MinM,theta,linear)
     R=(guess[0]-MinM[0])**2 + (guess[0]-MinM[0])**2
     R=sqrt(R)
-    S_output={'id': mediatrix_data[0]['id']}
+    S_output={'id': mediatrix_data['id']}
     try:
         S_output['MinM_norm']=MinM_val/(L*L)
     except:
-        print " Impossible to define a lenght for "+str(mediatrix_data[0]['id'])
+        print " Impossible to define a lenght for "+str(mediatrix_data['id'])
         S_output['MinM_norm']=-1
     try:
         S_output['L/R']=L/R
     except:
-        print "Impossible to define a radius and curvature for "+str(mediatrix_data[0]['id'])
+        print "Impossible to define a radius and curvature for "+str(mediatrix_data['id'])
         S_output['L/R']=-1
     if sigma_out==True:
         lim=round((L+2.)/2.,2)
@@ -121,14 +121,14 @@ def Evaluate_S_Statistic(mediatrix_data, sigma_out=True,sigma=1,sigma_pre=0.5,Ar
         try:
             S_output['sigma_lenght']=(sigma_lenght)/L
         except:
-            print "Impossible to define sigma_lenght for "+str(mediatrix_data[0]['id'])
+            print "Impossible to define sigma_lenght for "+str(mediatrix_data['id'])
         sigma_minor_axis=len(sigma_X)/(4*atan(1)*sigma_lenght)
         try:
             S_output['sigma_exc']=(sigma_lenght)/sigma_minor_axis
         except:
-            print "Impossible to define sigma_exc for "+str(mediatrix_data[0]['id'])
+            print "Impossible to define sigma_exc for "+str(mediatrix_data['id'])
         if Area_out==True:
-            image_name=mediatrix_data[0]['id']
+            image_name=mediatrix_data['id']
             image,hdr = pyfits.getdata(image_Path+image_name, header = True )
             pixels=numpy.where(image>0)
             sigma_points=[sigma_X,sigma_Y]
@@ -139,10 +139,114 @@ def Evaluate_S_Statistic(mediatrix_data, sigma_out=True,sigma=1,sigma_pre=0.5,Ar
     
 
 
+def Plot_S_Statistic(mediatrix_data, sigma_out=True,sigma=1,sigma_pre=0.5,Area_out=True,image_Path='', save=True,save_dir=''):
+
+    """
+    Function to make plot of S estatistic interesting measurements.
+    
+    Input:
+     - mediatrix_data <dic> : the output from mediatrix decomposition.
+
+     
+    Output:
+     - <bool> :   
+     
+    """
+
+
+    mediatrix_plot=print_mediatrix_Object_graph(mediatrix_data,image_dir=image_Path, keydots=True, colors= {'object': "g", 'vector': "b", 'keydots': "k"}, save=False)
+    guess=mediatrix_data['center']
+    Length=0
+    theta=[]
+    linear=[]
+    for i in range(0,len(mediatrix_data['origin'])):
+       origin_x=mediatrix_data['origin'][i][0]
+       origin_y=mediatrix_data['origin'][i][1]
+       end_x=mediatrix_data['end'][i][0]
+       end_y=mediatrix_data['end'][i][1]
+       Length_aux=(origin_x - end_x)**2 + (origin_y - end_y)**2
+       Length=Length+ sqrt(Length_aux)
+       delta_x=float((end_x-origin_x ))
+       if delta_x!=0:
+           a=float((end_y-origin_y ))/delta_x
+           theta.append(atan(a))
+           b=end_y-a*(end_x)
+           linear.append(b)
+       else:
+           theta.append(2*atan(1))
+           linear.append(end_y-origin_y)
+        
+    MinM=fmin(M_function,guess,args=(theta,linear),maxiter=1000, disp=0)
+    MinM_val=M_function(MinM,theta,linear)
+    MinM_plot_X=[MinM[0],MinM[0]]
+    MinM_plot_Y=[MinM[1],MinM[1]]
+    mediatrix_plot.plot(MinM_plot_Y,MinM_plot_X,"b.", zorder=2000)
+    lim=round((Length/8.)+5,2)
+    sigma_X=[]
+    sigma_Y=[]
+    X_search=arange(round(MinM[0],2)-lim, round(MinM[0],2)+lim,sigma_pre*1)
+    Y_search=arange(round(MinM[1],2)-lim, round(MinM[1],2)+lim,sigma_pre*1)
+    for j in range(0,len(Y_search)):
+        for i in range(0,len(X_search)):
+            p=[X_search[i],Y_search[j]]
+            M=  M_function(p,theta,linear) - MinM_val
+            
+            if M<=sigma:
+                X_aux=round(X_search[i],2)
+                Y_aux=round(Y_search[j],2)
+                p_aux=[X_aux,Y_aux]
+                rep=0
+                for k in range(0,len(sigma_X)):
+                    if sigma_X[k]==X_aux and sigma_Y[k]==Y_aux:
+                        rep+=1
+                if rep==0:
+                    sigma_X.append(X_aux)
+                    sigma_Y.append(Y_aux)
+    duplicates=[]
+    x_up=[]
+    x_down=[]
+    for i in range(0,len(sigma_Y)):
+        if len(duplicates)!=sigma_Y.count(sigma_Y[i]):
+            duplicates.append(sigma_X[i])
+        if len(duplicates)==sigma_Y.count(sigma_Y[i]):
+            duplicates=array(duplicates)
+            x_up.append(duplicates.max())
+            x_down.append(duplicates.min())
+            duplicates=[]
+    sigma_Y.sort()
+    last = sigma_Y[-1]
+    for i in range(len(sigma_Y)-2, -1, -1):
+        if last == sigma_Y[i]:
+            del sigma_Y[i]
+        else:
+            last = sigma_Y[i]
+
+
+   # mediatrix_plot.plot(sigma_Y,x_up,"m.")
+  #  mediatrix_plot.plot(sigma_Y,x_down,"m.")
+    mediatrix_plot.fill_between(sigma_Y, x_up, x_down, where=None, color="m", zorder=999)
+    
+           
+            #mediatrix_plot.plot(sigma_Y,sigma_X,"m.")
+
+    if save==True:
+        xmin, xmax = xlim()
+        ymin, ymax = ylim()
+        mediatrix_plot.set_xlim(xmin-1*Length,xmax+1*Length)
+        mediatrix_plot.set_ylim(ymin-1*Length,ymax+1*Length)
+        savefig(save_dir+mediatrix_data['id']+"_mediatrix_S_Estatistic_Graph.png")
+        mediatrix_plot.clear()
+        return True
+    else:
+        return mediatrix_plot
+
+
+
+
 def M_function(p,theta,b):
     M=0
     for i in range(0,len(theta)):
-	aux=Distance_from_line_to_point(p,theta[i],b[i])
+	aux=get_distance_from_line_to_point(p,theta[i],b[i])
         M=M+(aux*aux)
     M=M/float(len(theta))
     return M
