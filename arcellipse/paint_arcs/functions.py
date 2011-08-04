496f186a2e3c8b9f7e6e5d543fa2094e14b96421
Cristina Furlanetto <cristina.furlanetto@ufrgs.br>
2011-08-04 11:59:19 -0300

    Added new functions to compute the Einstein radius to the module functions.py

--- arcellipse/paint_arcs/functions.py
+++ arcellipse/paint_arcs/functions.py
@@ -3,7 +3,7 @@ from scipy import constants
 import math
 import re
 import string
-import pywcs
+#import pywcs
 import numpy
 
 def read_input_file(input_file):
@@ -104,28 +104,101 @@ def radec2xy(ra,dec,wcs):
 
 
 
-def proper_dist_int(z_1,omega_m,omega_lambda):
+def proper_dist_int(z,omega_m,omega_lambda):
 	'''Computes the integrand for the proper distance integral for a LCDM model with omega_k = 0.
-
+	
+	Input:
+	- z <float>: redshift
+	- h_0 <float>: Hubble constant / 100.
+	- omega_m <float>: matter density (relative to the critical density)
+	- omega_lambda <float>: density associated to the cosmological constant (relative to the critical density)
+	
+	Output:
+	- <float>: integrand for the proper distance integral
 	'''
 	
-	return 1./math.sqrt(omega_m*((1.+z_1)**3)+omega_lambda)
+	return 1./math.sqrt(omega_m*((1.+z)**3)+omega_lambda)
 
 
-def proper_dist(z,h_0,omega_m,omega_lambda):
+def proper_dist(z1,z2,h_0,omega_m,omega_lambda):
 
 	'''Computes the angular distance for a LCDM model with omega_k = 0
+	
+	Input:
+	- z1 <float>: observer redshift
+	- z2 <float>: objtect redshift
+	- h_0 <float>: Hubble constant / 100.
+	- omega_m <float>: matter density (relative to the critical density)
+	- omega_lambda <float>: density associated to the cosmological constant (relative to the critical density)
+	
+	Output:
+	- <float>: proper distance in Mpc
+	'''	
 
-	'''
-
-	c_h_0 = constants.c / (1000.* h_0) #(c in m/s; h_0 in km/s/Mpc; c_h_0 in Mpc)
+	c_h_0 = 3000./h_0 #(c_h_0 in Mpc)
 
 
-	return c_h_0 * quad(lambda z_1: proper_dist_int(z_1,omega_m,omega_lambda),0,z)[0]
+	return c_h_0 * quad(lambda z: proper_dist_int(z,omega_m,omega_lambda),z1,z2)[0]
 
-def angular_diameter_distance(z,h_0,omega_m,omega_lambda):
+def angular_diameter_distance(z1,z2,h_0,omega_m,omega_lambda):
+	'''Computes the angular diameter distance in Mpc using a LCDM model with omega_k = 0.
+	
+	Input:
+	- z1 <float>: observer redshift
+	- z2 <float>: objtect redshift
+	- h_0 <float>: Hubble constant / 100.
+	- omega_m <float>: matter density (relative to the critical density)
+	- omega_lambda <float>: density associated to the cosmological constant (relative to the critical density)
+	
+	Output:
+	- <float>: angular diameter distance in Mpc
+	'''	
+
+	return proper_dist(z1,z2,h_0,omega_m,omega_lambda)/ (1.+z2)
+
+
+def einstein_radius(m200,z1,z2,h_0,omega_m,omega_lambda):
+	'''Computes the Einstein radius in arcseconds for a singular isothermal sphere lens at z=z1
+	and source at z=z2, considering a LCDM model with omega_k = 0.
+	
+	Input:
+	- m200 <float>: mass within r200 (radius that encloses 200 times the critical density)
+	- z1 <float>: lens redshift
+	- z2 <float>: source redshift
+	- h_0 <float>: Hubble constant / 100.
+	- omega_m <float>: matter density (relative to the critical density)
+	- omega_lambda <float>: density associated to the cosmological constant (relative to the critical density)
+	
+	Output:
+	- <float>: Einstein radius in arcseconds
+	'''	
+
+	sigma = velocity_dispersion(m200,z1,h_0,omega_m,omega_lambda)
+	#sigma must be in km/s
+
+	dfrac = angular_diameter_distance(z1,z2,h_0,omega_m,omega_lambda)/angular_diameter_distance(0,z2,h_0,omega_m,omega_lambda)
+
+	return (4.*math.pi*dfrac*(sigma/300000)**2)*206264.81
 	
 
-	return proper_dist(z,h_0,omega_m,omega_lambda)/ (1.+z)
+def velocity_dispersion(m200,z,h_0,omega_m,omega_lambda):
 
+	'''Computes the velocity dispersion in km/s for a singular isothermal sphere at redshift z
+	using a LCDM model with omega_k = 0.
+	
+	Input:
+	- m200 <float>: mass within r200 (radius that encloses 200 times the critical density)
+	- z <float>: redshift
+	- h_0 <float>: Hubble constant / 100.
+	- omega_m <float>: matter density (relative to the critical density)
+	- omega_lambda <float>: density associated to the cosmological constant (relative to the critical density)
+	
+	Output:
+	- <float>: velocity dispersion in km/s
+	'''	
 
+	e_z = 1./(proper_dist_int(z,omega_m,omega_lambda))**2
+
+	return 475.*(m200*h_0*math.sqrt(200.)*e_z/(10e15))**(1./3.)
+
+	
