187a6f853d2b6dc6566acb26ee6f77f01ebd7fbc
Carlos Brandt <carloshenriquebrandt@gmail.com>
2011-08-10 10:44:05 -0300

    Update Lenzen pipeline

--- pipelines/finders/lenzen.py
+++ pipelines/finders/lenzen.py
@@ -20,8 +20,12 @@ class Config(dict):
     
     def __init__(self,imagefile=None,configfile=None):
         self.default()
-        self.sections = ['INPUT','SCALE_INPUT','SMOOTHING','DETECTION','OUTPUT']
-        self.head_lines = []
+        self.sections = [('INPUT',['Infile','Resize','x1','y1','x2','y2','Origin_x','Origin_y']),
+                        ('SCALE_INPUT',['Mode','Min','Max']),
+                        ('SMOOTHING',['Steps','Tau','K','Sigma','Rho','Outputfile','Outputformat']),
+                        ('DETECTION',['Minlevel','Mindiff','Minecc','Maxthick','Use_center_of_gc','Center_x','Center_y']),
+                        ('OUTPUT',['Extra_output','Outputfile','Data_directory','Catalogfile','Catalogtype'])]
+        self.head_lines = ["Temp_dir:   temp\n","Data_dir:   data\n"]
         self.begin_ini = 0
         if configfile:
             self.read_config(configfile)
@@ -54,9 +58,9 @@ class Config(dict):
                                 'Outputformat':'fits'
                             }
         self['DETECTION'] = {'Minlevel':'0.1',
-                                'Mindiff':'0.3',
-                                'Minecc':'0.7',
-                                'Maxthick':'30',
+                                'Mindiff':'0.1',
+                                'Minecc':'0.6',
+                                'Maxthick':'50',
                                 'Use_center_of_gc':'no',
                                 'Center_x':'',
                                 'Center_y':''
@@ -64,8 +68,8 @@ class Config(dict):
         self['OUTPUT'] = {'Extra_output':'none',
                             'Outputfile':'output_img.fits',
                             'Data_directory':'data/',
-                            'Catalogfile':'output_cat.fit',
-                            'Catalogtype':'large'
+                            'Catalogfile':'output_cat.txt',
+                            'Catalogtype':'small'
                             }
 
     def get(self,section,key):
@@ -80,6 +84,16 @@ class Config(dict):
             logging.error("Key %s not found in section %s" % (key,section))
             return
         sec[key] = str(value)
+        
+    def __head_lines__(self,cfg):
+        head_lines = [];
+        while True:
+            pos = cfg.tell();
+            line = cfg.readline();
+            if (line.find('[') >= 0) and (line.find('#') != 0): break;
+            if (line.rstrip('\n') != ''): head_lines.append(line);
+        self.head_lines = head_lines
+        self.begin_ini = pos
         
     def read_config(self,configfile):
         """ Returns a dictionary containing config sec->key:val structure """
@@ -96,173 +110,169 @@ class Config(dict):
                 sec[k.capitalize()] = re.sub('\s+','',re.sub('#.*','',v))
         cfg.close()
     
-    def __head_lines__(self,cfg):
-        head_lines = [];
-        while True:
-            pos = cfg.tell();
-            line = cfg.readline();
-            if (line.find('[') >= 0) and (line.find('#') != 0): break;
-            if (line.rstrip('\n') != ''): head_lines.append(line);
-        self.head_lines = head_lines
-        self.begin_ini = pos
-        
     def write_config(self,filename='input.par'):
         """ Function to write an INI file: [section] key:value """
-        fp = open(filename,'w')
-        [ fp.write(line+'\n') for line in self.head_lines ]
-        fp.close()
-        for section in self.sections:
-            fp = open(filename,'a')
-            _sec = self[section]
-            config.add_section(section);
-            for key,value in _sec.items():
-                config.set(section, key, value);
-        config.write(fp);
-        fp.seek(0);
         
+        fp = open(filename,'w')
+        [ fp.write(line) for line in self.head_lines ]
+        fp.close()
+        
+        fp = open(filename,'a')
+        for section,options in self.sections:
+            fp.write("\n")
+            _sec = self[section]
+            fp.write("["+section+"]\n")
+            for key in options:
+                fp.write("%s: %s\n" % (key.capitalize(),_sec[key]))
+        fp.close()
+        
+"""
+Temp_dir:      temp
+Data_dir:      data
 
-    
-def init(imagefile,eccentricity=0.7,outputimage='lenzen_out.fits',outputcat='lenzen_out.cat'):
+[INPUT]
+Infile: <?>                 # FITS or PGM file name
+Resize: no                  # 'yes' or 'no' 
+x1:                         # Rectangle to use
+y1:
+x2:
+y2:
+Origin_x: 0
+Origin_y: 0
 
-    # Atualizar variaveis:
-    # [INPUT] : 'Infile'
-    # [SCALE_INPUT] : 'Min' , 'Max'
-    # [OUTPUT] : 'Outputfile' , 'Catalogfile'
-    #
-    _config_large="""
-    Temp_dir:      temp
-    Data_dir:      data
+[SCALE_INPUT]
+Mode:  5                    # Scaling mode,recommended: 4 or 5   
+Min:   <?>                  # 'auto' or value
+Max:   <?>
+# Scaling mode:
+# 0 - no scaling
+# 1 - I(x)/Max, Intensities above Max are set to 1
+# 2 - intensities below Min are set to Min, above Max are set to Max
+# 3 - I(x)/Max ,intensities below Min are set to Min, above Max are set to 1
+# 4 - (I(x)-Min)/(Max-Min),intensities below Min are set to 0, above Max are set to 1
+# 5 -  sqrt((I(x)-Min)/(Max-Min)),intensities below Min are set to 0, above Max are set to 1
 
-    [INPUT]
-    Infile: %s                 # FITS or PGM file name
+[SMOOTHING]
+Steps:         1            # Numsteps*Tau define duration of diffusion process
+#                           # After each step the result can be stored in an outputfile (see below)
+Tau:           <?>          # typical range[1-15]
+K:             0.0001       # typical range[0.001-.00001]
+Sigma:         2            # typical range [1-5]
+Rho:           <?>          # typical value for arclets ~7-10, arcs ~10-20
+Outputfile:    none         # 'none' or filename without ending
+Outputformat:  pgm          # 'pgm' or 'fits'
 
-    Origin_x: 0
-    Origin_y: 0
+[DETECTION]
+Minlevel:      <?>          # [0,1] local maxima below are ignored
+Mindiff:                    # [0,1] minimal aquired difference between object and background
+#                           # default: same as Minlevel
+Minecc:        .7           # [0,1] minimal aquired eccenticity
+Maxthick:      10           # maximal thickness 
+Use_center_of_gc: no        # 'yes' or 'no'
+Center_x:                   # due to original image size
+Center_y: 
 
-
-    [SCALE_INPUT]
-    Mode:  4                    # Scaling mode,recommended: 4 or 5   
-    Min:   %s                  # 'auto' or value
-    Max:   %s
-
-    [SMOOTHING]
-    Steps:         1            # Numsteps*Tau define duration of diffusion process
-                                # After each step the result can be stored in an outputfile (see below)
-    Tau:           13           # typical range[1-15]
-    K:             0.0001       # typical range[0.001-.00001]
-    Sigma:         2            # typical range [1-5]
-    Rho:           15           # typical value for arclets ~7-10, arcs ~10-20
-
-    Outputfile:    smoothing    # 'none' or filename without ending
-    Outputformat:  pgm          # 'pgm' or 'fits'
-
-    [DETECTION]
-    Minlevel:      0.3          # [0,1] local maxima below are ignored
-    Mindiff:       0.3          # [0,1] minimal aquired difference between object and background
-    Minecc:        %s           # [0,1] minimal aquired eccentricity
-    Maxthick:      10           # maximal thickness 
-
-    [OUTPUT]
-    Extra_output:   none        # 'none' or file name
-                                # ending is automatically attachend (PGM/PPM)
-    Outputfile:     %s          # file name with fits- or PPM-ending
-    Data_directory: DATA/       # where to put the tempfiles, e.g. ./
-
-    Catalogfile:    %s        # 'none' or file name
-    Catalogtype:    small       # 'small' or 'large'
-
-    """
-    
-    os.system('mkdir temp data DATA');
-    os.system('ln -s ../%s data/.' % (imagefile));
-    param_file = 'input.par';
-    ecc = eccentricity;
-    outimg = outputimage;
-    outcat = outputcat;
-    fin = open(param_file,'w');
-    print >> fin, _config_large % (imagefile,'auto','99%',ecc,outimg,outcat)
-
-    return param_file;
+[OUTPUT]
+Extra_output:   none        # 'none' or file name
+#                           # ending is automatically attachend (PGM/PPM)
+Outputfile:     <?>         # file name with fits- or PPM-ending
+Data_directory: DATA/       # where to put the tempfiles, e.g. ./
+Catalogfile:    none        # 'none' or file name
+Catalogtype:    small       # 'small' or 'large'
+"""
 
 # --
+def run(imagefile=None, inputfile=None, clean=False):
+    """
+    Function to run Lenzen arc detector pipeline
+    
+    Input:
+     - imagefile   str : FITS image filename to segment
+     - inputfile   str : Lenzen's input filename (input.par)
+     - clean
+    
+    Output:
+     - Lenzen's code outputs. Stay calm, "ls" your dir ;P
+    
+    ---
+    """
+    
+    if (inputfile == None) and (imagefile == None):
+        logging.error("Neither an image file nor an input.par file given. Nothing to do.");
+        return False;
+    
+    config = Config(imagefile=imagefile,configfile=inputfile)
+    
+    imageinput = config.get('INPUT','Infile')
+    outputimage = re.sub(".fits","_out_img.fits",imageinput)
+    config.set('OUTPUT','Outputfile',outputimage)
+    outputcat = re.sub(".fits","_out_cat.txt",imageinput)
+    config.set('OUTPUT','Catalogfile',outputcat)
+    smoothfile = re.sub(".fits","_out_smooth",imageinput) 
+    config.set('SMOOTHING','Outputfile',smoothfile)
+    
+    lenzeninput = "run_lenzen.par"
+    config.write_config(lenzeninput)
+    
+    os.system('mkdir temp data &> /dev/null')
+    os.system('ln -sf ../%s data/. &> /dev/null' % (imageinput))
+    os.system('lenzen %s' % (lenzeninput))
+    
+    process_output(outputimage,outputcat,clean)
+    
+    return
 
+# --
 def process_output(imgfile,catfile,clean=False):
 
-    from scipy import ndimage;
-    import numpy as np;
+    from scipy import ndimage
+    import numpy as np
     
     # ----------
     # Catalog
-    os.system("sed -e 's/^[ ]*//' %s | tr -s ' ' > %s" % ('data/'+catfile,catfile[:-4]+'.txt'));
-    
-    columns = ['X','Y','ecc','lambda1','lambda2','A','B','theta','size'];
-    Dcatin = ascii_data.dict_from_csv(catfile[:-4]+'.txt',columns,header_lines=10,delimiter=' ');
+    catalog_file = catfile
+    os.system("sed -e 's/^[ ]*//' %s | tr -s ' ' > %s" % ('data/'+catfile,catalog_file))
+    columns = ['X','Y','ecc','lambda1','lambda2','A','B','theta','size']
+    Dcatin = ascii_data.dict_from_csv(catalog_file,columns,header_lines=10,delimiter=' ')
 
     # Lenzen outputs the 'Y' positions inverted. Lets fix that:
-    y_size = pyfits.getheader('data/'+imgfile)['naxis1'];
-    Y_inv = Dcatin['Y'];
-
-    Dcatin['Y'] = [ str(y_size-int(i)) for i in Y_inv ];
-    Catin = fits_data.dict_to_tbHDU(Dcatin);
-    print Dcatin.keys();
-    print Catin.data
-    # Select entries to output arc data
-    Catout = fits_data.sample_entries(Catin,ecc=0.7);
-    Catout.name = "Lenzen_arcsfound";
-    finalcat = 'Catalog_arcs_lenzen.out.fit';
-    Catout.writeto(finalcat,clobber=True);
-    # ----------
+    hdr = pyfits.getheader('data/'+imgfile)
+    y_size = hdr['naxis1']
+    x_size = hdr['naxis2']
+    Y_inv = Dcatin['Y']
     
-    # ----------
-    # Image
-    img = ndimage.imread('data/result.ppm');
-    comb = np.maximum(img[...,0],img[...,1]);
-    diff = img[...,2] - comb;
-    finalimg = 'Image_arcs_lenzen.out.fits';
-    pyfits.writeto(finalimg,diff[::-1],clobber=True)
-    # ----------
+    finalimg = re.sub("_out_img.fits","_out_result.fits",imgfile)
+    if len(Y_inv):
+        Dcatin['Y'] = [ str(y_size-int(i)) for i in Y_inv ]
+        Catin = fits_data.dict_to_tbHDU(Dcatin)
+
+        # Select entries to output arc data
+        Catout = fits_data.sample_entries(Catin,ecc=0.7)
+        Catout.name = "Lenzen_arcsfound"
+        finalcat = catalog_file[:-4]+'.fits'
+        Catout.writeto(finalcat,clobber=True)
+        
+        # Image
+        img = ndimage.imread('data/result.ppm')
+        comb = np.maximum(img[...,0],img[...,1])
+        diff = img[...,2] - comb
+        pyfits.writeto(finalimg,diff[::-1],clobber=True)
+        
+    else:
+        blank_array = np.zeros((y_size,x_size))
+        pyfits.writeto(finalimg,blank_array,clobber=True)
     
     if clean:
         os.system('rm -rf temp/ %s %s' % (imgfile,catfile));
 
-    return finalcat;
+    return
 
-# --
-
-def run(imgfile=None, inputfile=None, clean=False):
-
-    if inputfile == None:
-    
-        if imgfile == None:
-            logging.error("Neither an image file nor an input.par file given. Nothing to do.");
-            return False;
-        else:
-            inputfile = init(imgfile,ecc,outputimage,outputcat);
-
-    else:
-        pass;
-        
-    outputimage = 'lenzen_out.fits';
-    outputcat = 'lenzen_out.cat';
-    ecc = 0.7;
-    
-    # Initialize Horesh: read out necessary values from header
-    # and write it down to the input file.
-    if inputfile is None:
-        pass;
-        
-    os.system('lenzen %s' % (inputfile));
-    
-    finalcat = process_output(outputimage,outputcat);
-
-    return finalcat;
-    
 # ---
 if __name__ == '__main__':
 
     import optparse;
 
-    usage="\n  %prog <image.fits>"
+    usage="\n  %prog [options] <image.fits>"
     parser = optparse.OptionParser(usage=usage);
 
     parser.add_option('-p',
@@ -272,13 +282,13 @@ if __name__ == '__main__':
     (opts,args) = parser.parse_args();
 
     if len(args) == 0:
-        parser.error("No input images were given.");
-
-    inputfile = opts.input;
-
-    imgfile = args[0];
+        imgfile = None
+    else:
+        imgfile = args[0];
     
-    # Run Horesh's arcfinder
-    finalcat = run(imgfile,inputfile)
+    inputfile = opts.input;
+    
+    # Run Lenzen's arcfinder
+    run(imgfile,inputfile)
 
     sys.exit(0);
