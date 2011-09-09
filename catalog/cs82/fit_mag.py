7bf3d2e816e96fd5401e0ef179417bf9e8f92702
Bruno Moraes <brunomor@brunomor.(none)>
2011-09-09 20:00:45 -0300

    Add new magnitude cutting and output-creating functions

--- catalog/cs82/fit_mag.py
+++ catalog/cs82/fit_mag.py
@@ -16,18 +16,14 @@ def mag_function(m,a,b):
     return a*10**(b*m)
 
 
-def find_mag_lim(cut_inf_data,fit_params):
-    
-    a = fit_params[0]
-    b = fit_params[1]
+def find_mag_sup(data):
 
-    rel_diff = np.absolute(1-cut_inf_data[1]/mag_function(cut_inf_data[0],a,b))
-    
-    mag_mask = rel_diff > 0.1
+    if type(data) != 'numpy.ndarray':
+        data = np.array(data)
 
-    mag_lim = cut_inf_data[0][mag_mask].min()
+    mag_sup = data[0][data[1].argmax()] - 1
 
-    return mag_lim
+    return mag_sup
 
 
 def cut_mag_data(data, m_inf, m_sup):
@@ -55,11 +51,31 @@ def fit_mag_data(data,a,b):
     p0=[a,b]
 
     p, cov = optimize.curve_fit(mag_function, x, y, p0, yerror)
-    
+
     return p
 
 
+def find_mag_lim(cut_inf_data,fit_params):
+    
+    a = fit_params[0]
+    b = fit_params[1]
+
+    rel_diff = np.absolute(1-cut_inf_data[1]/mag_function(cut_inf_data[0],a,b))
+    
+    mag_mask = rel_diff > 0.1
+
+    mag_lim = cut_inf_data[0][mag_mask].min()
+
+    return mag_lim
+
+
+############################################## Pseudo Main Function
+
+
 X = open(sys.argv[1]).readlines()
+
+# Use later in the main function: data = np.transpose(np.loadtxt("/home/brunomor/Documents/Trabalho/CS82/CS82_SExtractor/Maria_Python_Code_Temporary/test_data.txt"))
+
 
 m, N, dN = [], [], []
 
@@ -70,13 +86,20 @@ for i in range(0,len(X)):
 
 data = [m, N, dN]
 
-cut_data = cut_mag_data(data,20,23.5)
+data = np.array(data)
+
+
+mag_sup = find_mag_sup(data)
+
+cut_data = cut_mag_data(data,20,mag_sup)
 
 cut_inf_data = cut_mag_data(data,20,99)
 
 p = fit_mag_data(cut_data,0,0.3)
 
 print p
+
+# Use later in the main function: np.savetxt("savetxt_test",np.transpose(cut_data),fmt='%f')
 
 fit_params = list(p)
 
