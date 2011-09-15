2737ee767927f4e0e9aced79566e435dec1b617c
Bruno Moraes <brunomor@brunomor.(none)>
2011-09-14 21:44:25 -0300

    Add options to path choosing and changes in the structure

--- catalog/cs82/mag_pipeline.py
+++ catalog/cs82/mag_pipeline.py
@@ -1,45 +1,93 @@
-# A first draft of the complete pipeline.
+from __future__ import division
+import sys
+import os
+import numpy as np
+import math
+import cmath
+from scipy import optimize
+import collections
+import fit_mag as fmg
+import glob
+import pyfits
 
-# Import necessary packages.
-
-def mag_pipeline(image_name(with_path), bin_size, gal_cut, mag99, mag_inf, S2N_cut, stell_idx):
+def mag_pipeline(input_file, field_names, mag_inf, bin_size, gal_cut, mag99, S2N_cut, stell_idx):
 
     # Get the data from the file
 
-    NUMBER, ALPHA_J2000, DELTA_J2000, X_IMAGE, Y_IMAGE, MAG_ISO, MAGERR_ISO, MAG_ISOCOR, MAGERR_ISOCOR, MAG_APER, MAGERR_APER, MAG_AUTO, MAGERR_AUTO, MAG_BEST, MAGERR_BEST, FLUX_RADIUS, CLASS_STAR, MU_MAX, FLAGS = np.loadtxt(filename, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18), unpack=True)
+    hdu = pyfits.open(input_file,ignore_missing_end=True)
 
-    data = np.array(data)
+    magdata = hdu[2].data.field(field_names[0])
+    magerrdata = hdu[2].data.field(field_names[1])
 
-    #cut_mag99
-   
-    #cut_stellarity
+    class_star = hdu[2].data.field('CLASS_STAR')
 
-    #cut_signal2noise
+    # Get the file number
 
-    binned_data = bin_mag_data(data, bin_size, m_inf)
+    image_name = input_file.replace('/home/brunomor/Documents/Trabalho/Repositorio/sltools/catalog/cs82/S82p28m_y.V2.3A.swarp.cut_', '')
+    image_number = float(image_name.replace('.fit', ''))
+    
+    
 
-    mag_sup = find_mag_sup(binned_data)
+    # Perform the cuts on the data
+    
+    if gal_cut == True:
+         stell_mask = (class_star < stell_idx)
 
-    cut_data = cut_mag_data(binned_data, m_inf, mag_sup)
+    else:
+         stell_mask = (class_star > stell_idx)
 
-    p = fit_mag_data(cut_data, a0, b0)
+
+    if mag99 == False:
+        mag99_mask = (magdata != 99)
+
+    else:
+        mag99_mask = (magdata != -1000)
+
+    # S2N_mask = magerrdata < 1.086/S2N_cut
+    
+
+    data = magdata[stell_mask & mag99_mask] # & S2N_mask]
+
+
+    # Bin and cut the data
+
+    binned_data = fmg.bin_mag_data(data, bin_size)
+
+    mag_sup = fmg.find_mag_sup(binned_data)
+
+    cut_data = fmg.cut_mag_data(binned_data, mag_inf, mag_sup)
+
+    # Perform fit and get limit magnitude 
+
+    a0 = 0;
+
+    b0 = 0.3;
+
+    p = fmg.fit_mag_data(cut_data, a0, b0)
 
     fit_params = list(p)
 
-    cut_inf_data = cut_mag_data(binned_data,m_inf,99)
+    cut_inf_data = fmg.cut_mag_data(binned_data,mag_inf,99)
 
-    find_mag_lim(cut_inf_data,fit_params)
+    mag_lim = fmg.find_mag_lim(cut_inf_data,fit_params)
 
-# Make the plots and save them to a given folder.
+    # Make the plots and save them to a given folder. (still to do)
+
+    # Return results of the fit
+
+    return [image_number, fit_params[0], fit_params[1], mag_inf, mag_sup, mag_lim]
 
 
-# Create the main function: include the necessary options (different cuts, inputs, etc with parser blablabla).
+# ==========================
 
 if __name__ == "__main__" :
 
+    '''
     from optparse import OptionParser;
 
-    parser = OptionParser();
+    usage="\n  %prog [flags] [options] ??? ??? "
+    
+    parser = OptionParser(usage=usage);
 
     parser.add_option('-b','--bin_size', dest='bin_size', default=0.5, help='bla');
 
@@ -48,8 +96,6 @@ if __name__ == "__main__" :
     parser.add_option('-g','--galaxy_cut', dest='gal_cut', default=True, help='bla');
 
     parser.add_option('-i','--include_unidentified_magnitudes', dest='mag99', default=False, help='bla'); #Check this definition later
-
-    parser.add_option('-m','--mag_inf_cut', dest='m_inf', default=19, help='bla'); #Input obrigatório
 
     parser.add_option('-n','--signal2noise_cut', dest='S2N_cut', default=5, help='bla');
 
@@ -61,9 +107,10 @@ if __name__ == "__main__" :
     bin_size = opts.bin_size;
     gal_cut = opts.galaxy_cut;
     mag99 = opts.mag99;
-    m_inf = opts.m_inf;
     S2N_cut = opts.S2N_cut;
     stell_idx = opts.stell_idx;
+
+    # if m_inf is not given, take the minimum for which N = 10.
 
     if ( opts=={} ):
         print"";
@@ -80,18 +127,34 @@ if __name__ == "__main__" :
 # Include here a series of tests for the optional parameters to avoid inconsistencies.
 
 # Get all the files in the given folder.
+'''
+    bin_size = 0.5;
+    gal_cut = True;
+    mag99 = False;
+    S2N_cut = 5;
+    stell_idx = 0.8;
 
-    filenames = glob.glob('*.cat')
+    filenames = glob.glob(sys.argv[1])
+
+    field_names = [sys.argv[2],sys.argv[3]]
+
+    mag_inf = float(sys.argv[4])
 
 # Run the program for each file and send the results to the folder in question. The sending of plots is inside the pipeline, the sending of the final results for all files is here.
 
+    results = []
+
     for i in range(len(filenames)):
-        
-        result = mag_analysis(path_name_filenames[i],...)
+    
+        print mag_pipeline(filenames[i], field_names, mag_inf, bin_size, gal_cut, mag99, S2N_cut, stell_idx)
+    '''    
+        results.append(mag_pipeline(filenames[i], field_names, mag_inf, bin_size, gal_cut, mag99, S2N_cut, stell_idx))
+ 
+    results = np.array(results)
 
-        "savetxt(result) bla bla to the necessary folder"
-
-        return
+    print results
+    '''
+    # The savetxt command must be here.
 
 
     sys.exit(0);
