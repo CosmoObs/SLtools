ec8c96c4067833868ccd9e5654cc9de01c75e422
Bruno Moraes <brunomor@brunomor.(none)>
2012-02-22 13:45:01 -0200

    Commit new features (undocumented, not debugged yet)

--- catalog/cs82/mag_pipeline.py
+++ catalog/cs82/mag_pipeline.py
@@ -14,6 +14,7 @@ import pyfits
 
 sys.path.append("/home/brunomor/Documents/Trabalho/Repositorio") ### Append Bruno
 from sltools.catalog import fits_data as fd
+from sltools.coordinate import wcs_conversion as wcscv
 
 
 def mkdir_p(path):
@@ -27,6 +28,72 @@ def mkdir_p(path):
 
 def mag_pipeline(input_file, field_names, folder_path, mag_inf, bin_size, gal_cut, mag99, S2N_cut, stell_idx, plot_inf, plot_sup):
 
+
+#######################################################
+
+    hdudata = pyfits.open(input_file,ignore_missing_end=True)[2].data
+    hdulist_string = pyfits.open(input_file,ignore_missing_end=True)[1].data[0][0]
+
+    tile_ra = fmg.get_entry_tile_cs82_ldac(hdulist_string,'CRVAL1')
+    tile_dec = fmg.get_entry_tile_cs82_ldac(hdulist_string,'CRVAL2')
+    tile_seeing = fmg.get_entry_tile_cs82_ldac(hdulist_string,'SEEING')
+    
+    tile_name = fmg.get_tile_name(input_file)
+    tile_area = (21000*0.187/60)**2 # in arcmin^2
+ 
+    magdata = hdudata.field(field_names[0])
+    magdata = magdata.astype(np.float64)
+    
+    magerrdata = hdudata.field(field_names[1])
+    magerrdata = magerrdata.astype(np.float64)
+ 
+    class_star = hdudata.field('CLASS_STAR')
+    class_star = class_star.astype(np.float64)
+
+    flags = hdudata.field('FLAGS')
+
+     
+    if gal_cut == True:
+        stell_mask = (class_star < stell_idx)
+ 
+    else:
+        stell_mask = (class_star > stell_idx)
+
+ 
+    if mag99 == False:
+        mag99_mask = (magdata != 99)
+ 
+    else:
+        mag99_mask = True
+
+    flag_mask = (flags == 0) 
+
+    S2N_mask = magerrdata < 1.086/S2N_cut
+     
+    objs = []
+ 
+    objs.append(len(magdata))
+    objs.append(len(magdata[stell_mask]))
+    objs.append(len(magdata[~stell_mask]))
+    objs.append(len(magdata[stell_mask & mag99_mask]))
+ 
+ 
+    data = magdata[stell_mask & mag99_mask & S2N_mask]
+
+    objs.append(len(data))
+
+    objs.append(len(magdata[flag_mask]))
+ 
+    # Bin and cut the data
+
+ 
+    binned_data = fmg.bin_mag_data(data, bin_size)
+
+#######################################################
+
+# Versao lenta demais com o sample_entries
+
+    '''
     # Get the data from the file and the tile name
 
 
@@ -35,6 +102,12 @@ def mag_pipeline(input_file, field_names, folder_path, mag_inf, bin_size, gal_cu
 
     hdu = pyfits.open(input_file,ignore_missing_end=True)[2]
 
+    # Count objects in different cuts
+    
+    objs = []
+
+    objs.append(len(hdu.data))
+
    
     # Perform the cuts on the data
 
@@ -42,38 +115,37 @@ def mag_pipeline(input_file, field_names, folder_path, mag_inf, bin_size, gal_cu
     # Stellarity cut
     
     if gal_cut == True:
-         hdu_stell_cut = fd.sample_entries(hdu,CLASS_STAR=(0,stell_idx))
+         hdu = fd.sample_entries(hdu,CLASS_STAR=(0,stell_idx))
 
     else:
-         hdu_stell_cut = fd.sample_entries(hdu,CLASS_STAR=(stell_idx,1))
+         hdu = fd.sample_entries(hdu,CLASS_STAR=(stell_idx,1))
+
+    objs.append(len(hdu.data))
 
 
     # Mag != 99 cut
 
     if mag99 == False:
-        hdu_stell_mag99_cut = fd.sample_entries(hdu_stell_cut,MAG_AUTO=(0,98))
+        hdu = fd.sample_entries(hdu,MAG_AUTO=(0,98))
 
-    else:
-        hdu_stell_mag99_cut = hdu_stell_cut
+    objs.append(len(hdu.data))
 
     
     # S/N cut
 
-    hdu_all_cuts = fd.sample_entries(hdu_stell_mag99_cut, MAGERR_AUTO=(0,1.086/S2N_cut))
+    hdu = fd.sample_entries(hdu, MAGERR_AUTO=(0,1.086/S2N_cut))
+
+    objs.append(len(hdu.data))
     
-
-    # Count objects in different cuts
-    
-    objs = [len(hdu.data),len(hdu_stell_cut.data),len(hdu_stell_mag99_cut.data),len(hdu_all_cuts.data)]
-
-
 
     # Bin the data and get rid of undesired bins
     
-    magdata = hdu_all_cuts.data.field(field_names[0])
+    magdata = hdu.data.field(field_names[0])
     magdata = magdata.astype(np.float64)
 
     binned_data = fmg.bin_mag_data(magdata, bin_size)
+    '''
+####################################################################
 
     binned_data = fmg.cut_unpopulated_bins(binned_data, 10) # Attention! Hard-coded value for the minimum number of counts for keeping bins 
 
@@ -100,17 +172,37 @@ def mag_pipeline(input_file, field_names, folder_path, mag_inf, bin_size, gal_cu
 
     mag_lim = fmg.find_mag_lim(cut_inf_data, fit_params, bin_size)
 
-    # Return results of the fit
-
-
-    return [tile_name, str(bin_size), str(S2N_cut), str(fit_params[0]), str(fit_params[1]), str(mag_inf), str(mag_sup), str(mag_lim), str(objs[0]), str(objs[1]), str(objs[2]), str(objs[3])]
-
-
+    
     # Make the plots and save them to a given folder
-'''
-    fmg.make_plot(binned_data, field_names, folder_path, fit_params, mag_inf, mag_sup, mag_lim, bin_size, gal_cut, S2N_cut, image_number, plot_inf, plot_sup)
+    
+    mkdir_p(folder_path + '/Plots/')
 
-'''
+    fmg.make_plot_mag_pipe(binned_data, tile_name, folder_path, fit_params, mag_inf, mag_sup, mag_lim, bin_size, gal_cut, S2N_cut, stell_idx, plot_inf, plot_sup)
+    
+    # Return results of the fit
+    
+    hdulist_string = 0
+    hdudata = 0
+    magdata = 0
+    magerrdata = 0
+    classstar = 0
+    data = 0
+    cut_inf_data = 0
+    cut_data = 0
+    binned_data = 0
+    cut_inf_data = 0
+    stell_mask = 0
+    mag99_mask = 0
+    S2N_mask = 0
+    flags = 0
+    flag_mask = 0
+    
+    del hdudata
+    del hdulist_string
+
+
+    return [tile_name, str(tile_ra), str(tile_dec), str(tile_seeing), str(bin_size), str(S2N_cut), str(stell_idx), str(fit_params[0]), str(fit_params[1]), str(mag_inf), str(mag_sup), str(mag_lim), str(objs[0]), str(objs[1]), str(objs[3]), str(objs[4]),str(objs[5]/objs[0]),str(objs[1]/tile_area),str(objs[2]/tile_area)]
+
 
 # ==========================
 
@@ -176,32 +268,34 @@ if __name__ == "__main__" :
     results = []
 
     for i in range(len(filenames)):
+
+        print i
        
         results.append(mag_pipeline(filenames[i], field_names, folder_path, mag_inf, bin_size, gal_cut, mag99, S2N_cut, stell_idx, plot_inf, plot_sup))
 
     results = np.array(results)
 
 
-    np.savetxt(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+'.txt', results,fmt='%s       %s         %s       %.8s       %.5s       %s        %s        %s      %s      %s      %s      %s')
+    np.savetxt(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+ '_stell_'+str(stell_idx)+'.txt', results,fmt='%7s       %s        %s        %.4s      %s         %s       %.4s     %.8s       %.5s       %s        %s        %s           %s      %6s      %6s      %6s      %s      %s      %s')
 
-    # np.savetxt(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+'.txt',results,fmt='%c          %-3.2f         %.0f       %f    %.4f     %.2f      %.2f      %.2f      %-6.0f      %-6.0f        %-6.0f        %-6.0f')
+    # np.savetxt(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+ '_stell_'+str(stell_idx)+'.txt',results,fmt='%7s       %s       %s        %.4s      %s         %s       %.4s     %.8s       %.5s       %s        %s        %s           %s      %6s      %6s      %6s      %s      %s      %s')
 
     # Numpy 1.5 doesn't have header options for savetxt yet (to come in Numpy 2.0). Add a header using file commands
     
-    S''' 
-    file = open(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+'.txt', "r+")
+     
+    file = open(folder_path + '/fit_results_bins_'+ str(bin_size) + '_S2N_'+ str(int(S2N_cut))+ '_stell_'+str(stell_idx)+'.txt', "r+")
      
     old = file.read() # read everything in the file
      
     file.seek(0) # rewind
 
     if gal_cut:
-        file.write("ID_test    bin_size    S/N_cut        A_fit         b_fit     mag_inf     mag_sup    mag_comp    N_tot        N_gal      N_gal_99    N_gal_99_S/N \n" + old)
+        file.write("ID_test       ra         dec         seeing    bin_size    S/N_cut    class_star     A_fit         b_fit     mag_inf     mag_sup    mag_comp    N_tot        N_gal      N_gal_99    N_gal_99_S/N      N_flags/N_tot    N_gal/arcmin^2       N_star/arcmin^2\n" + old)
 
     else:
-        file.write("ID_test    bin_size    S/N_cut        A_fit         b_fit     mag_inf     mag_sup    mag_comp    N_tot        N_star      N_star_99    N_star_99_S/N \n" + old)
+        file.write("ID_test       ra         dec         seeing    bin_size    S/N_cut    class_star     A_fit         b_fit     mag_inf     mag_sup    mag_comp    N_tot        N_star      N_star_99    N_star_99_S/N     N_flags/N_tot    N_star/arcmin^2      N_star/arcmin^2\n" + old)
      
     file.close()
-    '''
+    
 
     sys.exit(0);
