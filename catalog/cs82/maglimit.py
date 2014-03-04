2c09b84beed41f417363b2c57788904b4bbc05de
Bruno Moraes <bruno.moraes@cbpf.br>
2014-03-04 11:42:17 -0300

    Correct output for 'None' fit limit input. Add input handling. Update documentation.

--- catalog/cs82/maglimit.py
+++ catalog/cs82/maglimit.py
@@ -10,6 +10,7 @@
 
 from __future__ import division
 import numpy as np
+import sys
 from scipy.optimize import curve_fit
 
 
@@ -19,9 +20,9 @@ def create_counts_data(mag_data, binsize):
 
     Input:
      - mag_data        np.array : Magnitude data array
-     - binsize           float : Bin size
+     - binsize           f loat : Bin size
     Output:
-     - counts_data    np.array : [bin values, counts, Poisson errors]
+     - counts_data     np.array : [bin values, counts, Poisson errors]
     """
 
     # Define bin limits to be closest lower/higher int to mag_data min/max
@@ -56,32 +57,25 @@ def counts_model(mag, a, b):
     return a*10**(b*mag)
 
 
-def _fit_counts_data(counts_data, initparams=(1, 0.3), minf=None, msup=None):
+def _fit_counts_data(counts_data, fitlimits, initparams=(1, 0.3)):
 
     """
     Fit count data to analytical model in a selected magnitude range.
 
-    If minf and msup are equal to None, the fitting range is automatically
-    calculated using the peak of the counts distribution as a reference
-    point.
-
     Input:
      - counts_data    np.array : Counts in the format [bins, counts, errors]
+     - fitlimits         tuple : Fitting range limits in format (minf, msup)
      - initparams        tuple : Initial parameter guesses
-     - minf              float : Fitting range inferior limit
-     - msup              float : Fitting range superior limit
     Output:
      - fitparams      np.array : Fitted parameters in format [afit, bfit]
     """
 
-    if minf is None:
-        minf = counts_data[0][counts_data[1].argmax()] - 3
+    # assert something about fit limits and about initparams - Add later
 
-    if msup is None:
-        msup = counts_data[0][counts_data[1].argmax()] - 1 
+    minf = fitlimits[0]
+    msup = fitlimits[1]
 
-    # Bug if the user doesn't pass a valid int or float
-    mask = (counts_data[0] > float(minf)) & (counts_data[0] < float(msup))
+    mask = (counts_data[0] > minf) & (counts_data[0] < msup)
     counts_data = counts_data[:, mask]
 
     # Perform fit
@@ -118,8 +112,8 @@ def _calc_rel_diff(counts_data, fitparams):
     return rel_diff
 
 
-def get_mag_limit_counts(mag_data, binsize, initparams=(1, 0.3), minf=None,
-                         msup=None, comp_limit=0.8):
+def get_mag_limit_counts(mag_data, binsize, minf=None, msup=None,
+                         initparams=(1, 0.3), comp_limit=0.8):
     """
     Get magnitude limit from the number counts in magnitude bins.
 
@@ -134,12 +128,16 @@ def get_mag_limit_counts(mag_data, binsize, initparams=(1, 0.3), minf=None,
     obtains the magnitude value at which completeness drops below a given
     threshold. It returns this magnitude value.
 
+    If minf and msup are equal to None, the fitting range is automatically
+    calculated using the peak of the counts distribution as a reference
+    point.
+
     Input:
      - mag_data      np.array : Magnitude data array
      - binsize          float : Bin size
-     - initparams       tuple : Initial parameter guesses
      - minf             float : Fitting range inferior limit
      - msup             float : Fitting range superior limit
+     - initparams       tuple : Initial parameter guesses
      - comp_limit       float : Completeness limit
     Output:
      - maglim           float : Magnitude limit
@@ -149,17 +147,35 @@ def get_mag_limit_counts(mag_data, binsize, initparams=(1, 0.3), minf=None,
      - msup             float : Fitting range superior limit
     """
 
-    # Defend against bad input - Add code later
+    # Defend against bad input - Add more code here later
 
     counts_data = create_counts_data(mag_data, binsize)
 
-    kwargsfit = {'initparams': initparams}
-    if minf is not None:
-        kwargsfit['minf'] = minf
-    if msup is not None:
-        kwargsfit['msup'] = msup
+    if minf is None:
+        minf = counts_data[0][counts_data[1].argmax()] - 3
 
-    fitparams = _fit_counts_data(counts_data, **kwargsfit)
+    if msup is None:
+        msup = counts_data[0][counts_data[1].argmax()] - 1
+
+    try:
+        fitlimits = (float(minf), float(msup))
+    except (ValueError, TypeError) as err:
+        print sys.stderr, err
+        print ""
+        print sys.stderr, "minf and msup must each be a single float or int."
+        sys.exit(1)
+
+    if fitlimits[1] <= fitlimits[0]:
+        print sys.stderr, "msup must be larger than minf. Exiting..."
+        sys.exit(1)
+
+    if not (0.0 < comp_limit < 1.0):
+        print sys.stderr, "comp_limit must be a float between 0 and 1."
+        sys.exit(1)
+        
+
+    fitparams = _fit_counts_data(counts_data, fitlimits, 
+                                 initparams=initparams)
 
     rel_diff = _calc_rel_diff(counts_data, fitparams)
 
@@ -172,4 +188,4 @@ def get_mag_limit_counts(mag_data, binsize, initparams=(1, 0.3), minf=None,
     #if plot is not None:
 
     return [maglim, fitparams[0], fitparams[1], minf, msup]
-
+    
