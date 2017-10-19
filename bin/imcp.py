bb6764e319e509a8862a367792e5a753782624e5
Iruatã Souza <iru.muzgo@gmail.com>
2017-10-19 07:47:24 -0200

    bin/imcp: distinct arguments for distinct tasks

--- bin/imcp.py
+++ bin/imcp.py
@@ -4,12 +4,13 @@ import sys
 import os
 import pyfits
 import string
+import re
 
 from optparse import OptionParser
 from sltools.io import log
 from sltools.image import imcp
 
-usage = "%prog [options] image.fits x y"
+usage = "%prog [options] image.fits [x y]"
 parser = OptionParser(usage = usage)
 
 parser.add_option('--debug',
@@ -32,10 +33,12 @@ parser.add_option('-s',
     dest = 'shape', default = '250,250',
     help = "Output image shape. Default is 250,250")
 
+# XXX is this option really used?
 parser.add_option('--increase',
     dest = 'incr', default = 0,
     help = "Amount of border (enlarge) of object poststamp. It is an additive or multiplicative factor. See 'relative_increase' flag")
 
+# XXX is this option really used?
 parser.add_option('--relative_increase',
     action = 'store_true', dest = 'rel_incr', default = False,
     help = "Turn on the multiplicative factor for increase image, otherwise, 'increase' will be treated as an absolute value.")
@@ -60,55 +63,65 @@ parser.add_option('--hdu-n', default = 0, help = "HDU position")
 
 (opts, args) = parser.parse_args()
 
-if len(args) < 3:
-    parser.print_help()
-    sys.exit(0)
-
 logging = log.init(debug = opts.debug, verbose = opts.verbose)
 
-segimg = opts.segimg
+nrequired = opts.segimg and 1 or 3
+if len(args) < nrequired:
+    parser.print_help()
+    sys.exit(1)
+
 IDs = opts.IDs
-incr = opts.incr
-rel_incr = opts.rel_incr
-use_header = opts.use_header
-coord_deg = opts.is_coord_deg
-shape_deg = opts.is_shape_deg
-dx, dy = string.split(opts.shape, sep = ',')
 prefix = opts.prefix
 
 infits = args[0]
-x = args[1]
-y = args[2]
 
-coord_unit = 'degrees' if coord_deg else 'pixel'
-size_unit = 'degrees' if shape_deg else 'pixel'
+coord_unit = opts.is_coord_deg and 'degrees' or 'pixel'
+size_unit = opts.is_shape_deg and 'degrees' or 'pixel'
 
 hdulist = pyfits.open(infits, memmap = True)
 
-if use_header:
+if opts.use_header:
     image = hdulist[int(opts.hdu_n)].data
     hdr = hdulist[int(opts.hdu_n)].header
 else:
     image = hdulist[int(opts.hdu_n)].data
     hdr = None
 
-if segimg:
-    IDs = regexp.str2lst(IDs)
-    images, headers = imcp.segstamp(image, segimg, hdr = hdr, increase = incr, relative_increase = rel_incr, objIDs = objIDs)
+if opts.segimg:
+    IDs = [ int(ID) for ID in re.split(',', opts.IDs) ]
+
+    seghdu = pyfits.open(opts.segimg, memmap = True)
+    seg = seghdu[0].data
+    hdr = seghdu[0].header
+    print(opts.segimg)
+
     if prefix == '':
         prefix = 'pstamp'
 
     for i in range(len(IDs)):
       outpath = '%s_%s.fits' % (prefix, IDs[i])
+      print opts.segimg, infits
+
+      image, header = imcp.segstamp(seg, IDs[i], objimg = image,
+          hdr = hdr, increase = opts.incr, relative_increase = opts.rel_incr)
 
       try:
-          pyfits.writeto(outpath, images[i], headers[i])
+          pyfits.writeto(outpath, image, header)
       except IOError as e:
           sys.stderr.write('error: %s\n' % e)
-          sys.exit(0)
+          sys.exit(1)
 
 else:
-    image, header = imcp.cutout(image, hdr = hdr, coord_unit = coord_unit, xo = x, yo = y, size_unit = size_unit, x_size = dx, y_size = dy)
+    if len(args) < 3:
+        parser.print_help()
+        sys.exit(1)
+
+    x = args[1]
+    y = args[2]
+    dx, dy = string.split(opts.shape, sep = ',')
+
+    image, header = imcp.cutout(image, hdr = hdr, coord_unit = coord_unit,
+        xc = x, yc = y, size_unit = size_unit, x_size = dx, y_size = dy)
     if prefix == '':
         prefix = 'cut'
     outpath = '%s.fits' % prefix
