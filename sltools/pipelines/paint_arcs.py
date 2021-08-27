56c1d5e8ddbd9a73ca5b5f90c3a6f3eecf783ed0
Eduardo Valadão <eduardovaladao98@gmail.com>
2021-08-27 19:18:12 -0300

    modified for new version of AstroPy

--- sltools/pipelines/paint_arcs.py
+++ sltools/pipelines/paint_arcs.py
@@ -2,55 +2,55 @@
 # =====================================================
 # Authors:
 # Cristina Furlanetto - furlanetto.cristina@gmail.com
+# Eduardo Valadão - eduardovaladao98@gmail.com
 # =====================================================
+ 
+import math
+import convolve 
+import add_noise
+import numpy as np 
+import paint_arcs_v1_1 as pa
+import astropy.io.fits as pyfits
+from convert_arcsec_pix import convert_arcsec_pix
 
-def run(params,seeing,background,suffix_name=''):
+def run(params, seeing, background, pix_scale, suffix_name=''):
 
-    hdu = paint_arcs(params)
+    hdu = pa.paint_arcs(params, pix_scale)
     
     arc_array = hdu.data
     arc_header = hdu.header
     
-    pyfits.writeto("arc"+suffix_name+".fits",arc_array,arc_header,clobber=True)
-    
+    pyfits.writeto("arc"+suffix_name+".fits", arc_array, arc_header, overwrite=True)
+     
     dim_x = arc_header["NAXIS1"]
     dim_y = arc_header["NAXIS2"]
     
     # Convolving the arc
-    seeing = convert_arcsec_pix(seeing,pix_scale)	
-    sigma = seeing / (2.*math.sqrt(2.*math.log(2.)))
-    arc_header.update('APSF',seeing,comment='Arc psf (arcsec)')	
+    seeing = convert_arcsec_pix(seeing, pix_scale)	
+    sigma = seeing/(2.0*math.sqrt(2.0*math.log(2.0)))
+    arc_header['APSF'] = (seeing, 'Arcs PSF (arcsec)')	
     
-    arc_conv_array = convolve.gauss_convolution_fft(arc_array,3.,sigma)
-    pyfits.writeto("arc_conv"+suffix_name+".fits",arc_conv_array,arc_header,clobber=True)
+    arc_conv_array = convolve.gauss_convolution_fft(arc_array, 3.0, sigma)
+    pyfits.writeto("arc_conv"+suffix_name+".fits", arc_conv_array, arc_header, overwrite=True)
         
     # Adding Poisson noise
     arc_conv_noise_array = add_noise.add_poisson_noise(arc_conv_array)
-    pyfits.writeto("arc_conv_noise"+suffix_name+".fits",arc_conv_noise_array,arc_header,clobber=True)	
+    pyfits.writeto("arc_conv_noise"+suffix_name+".fits", arc_conv_noise_array, arc_header, overwrite=True)	
     
     # Creating background image with noise
-    bkg_array = background * numpy.ones((dim_y,dim_x)) 
+    bkg_array = background*np.ones((dim_y,dim_x)) 
     
     bkg_noise_array = add_noise.add_poisson_noise(bkg_array)
     
-    pyfits.writeto("background"+suffix_name+".fits",bkg_noise_array,clobber=True)
+    pyfits.writeto("background"+suffix_name+".fits", bkg_noise_array, overwrite=True)
     
     # Adding arc to the background image
     arc_conv_noise_bkg = arc_conv_noise_array + bkg_noise_array
-    arc_header.update('BKG',background,comment='Background mean value')	
+    arc_header['BKG'] = (background, 'Background Mean Value')	
     
-    pyfits.writeto("arc_conv_noise_bkg"+suffix_name+".fits",arc_conv_noise_bkg,arc_header,clobber=True)	
-
-    
-
-#params = [7.,1.,0.6,30.,230.,20.,100.,1000.,31.04]
-#params = [7.,1.,0.6,30.,230.,20.,100.,1000.,31.04]
-#params = [10.,4.,0.6,100.,300.,20.,100.,1000.,31.04]
+    pyfits.writeto("arc_conv_noise_bkg"+suffix_name+".fits", arc_conv_noise_bkg, arc_header, overwrite=True)	
 
 
-
-
-
-
-#run(params,0.6,500.)
+params = [0.6, 4.75, 4.0, 90, 1, 0.12, 0.8, 20, 31.83, 0, 0, 0, 4, 4, 4, 2]
+run(params, 0.5, 75, 0.04, "einstein3")
 
